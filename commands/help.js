2d0417ac5bf337b905bfd92c502951b1be03eb4d
Dan-jersey <stromjersey@gmail.com>
2025-01-15 23:54:05 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports = (bot) => {
             { command: '/start', description: 'démarrage du bot' },
             { command: '/getid', description: 'Obtenez votre ID Telegram' },
             { command: '/imgbb', description: 'transforme les photos en lien' },
-            { command: '/Dan jersey', description : 'sur Dan jersey' },
+            { command: '/Dan', description : 'sur Dan jersey' },
        
         ];
 
